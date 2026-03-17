0332ad45b39fe0a00f7cd35346898409fa76cc95
Sam Indiazi <saindiazi@gmail.com>
2026-03-16 21:04:40 -0500

    Work changes - added maintenance feature and unit images

--- src/main/java/com/example/rentalmanager/maintenance/infrastructure/web/controller/MaintenanceController.java
+++ src/main/java/com/example/rentalmanager/maintenance/infrastructure/web/controller/MaintenanceController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/maintenance")
 @RequiredArgsConstructor
-public class PusMaintenanceController {
+public class MaintenanceController {
 
     private final CreateMaintenanceRequestUseCase createUseCase;
     private final UpdateMaintenanceRequestUseCase updateUseCase;
